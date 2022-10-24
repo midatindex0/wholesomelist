59c45a4d28558bec0c7880dea1cca335a997f398
Lonely-code-cube <lonely-code-cube@github.com>
2022-10-24 21:40:48 +0530

    Added metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 	<title>Wholesomelist - DreamH</title>
 	<meta content="DreamH Team" property="og:author" />
 	<meta content="Wholesome Hentai God List (DreamH UI)" property="og:title" />
+	<meta
+		content="'The god list is the best source for verified wholesome hentai: here, we find, tag, and tier the most wholesome of hentai so you don't have to.' - wholesomelist.com"
+		property="og:description"
+	/>
 	<meta content="https://wholesomelist.vercel.app/" property="og:url" />
 	<meta content="https://wholesomelist.vercel.app/dreamh.png" property="og:thumbnail" />
 </svelte:head>

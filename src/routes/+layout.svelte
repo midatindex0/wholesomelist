daab8383647432114a7936d59ccc722dbb4ae492
Lonely-code-cube <lonely-code-cube@github.com>
2022-10-24 21:36:18 +0530

    Added metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Wholesomelist - DreamH</title>
+	<meta content="DreamH Team" property="og:author" />
+	<meta content="Wholesome Hentai God List (DreamH UI)" property="og:title" />
+	<meta content="https://wholesomelist.vercel.app/" property="og:url" />
+	<meta content="https://wholesomelist.vercel.app/dreamh.png" property="og:thumbnail" />
+</svelte:head>
+
 <section>
 	<slot />
 </section>
